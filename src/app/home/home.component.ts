23fd63cfb1dde5d98b7f0473d9ceb96cf39a2057
Danny Gione <thegiones13@sbcglobal.net>
2025-05-16 23:41:59 -0700

    Found bug in native search, not returning truly native things, and is also returning naturalized plants as native

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GbifService } from '../services/gbif.service';
 import { BehaviorSubject, Observable, Subject, switchMap, takeUntil, tap } from 'rxjs';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { GBIFPageableResult } from '../models/gbif.pageable-result';
+import { GbifOccurrence } from '../models/gbif.occurrence';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +16,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   private _ngDestroy$: Subject<void> = new Subject<void>();
   private _positionEmitter$: Subject<GeolocationPosition> = new Subject<GeolocationPosition>();
-  private _lastSearch$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  private _lastSearch$: BehaviorSubject<GBIFPageableResult<GbifOccurrence> | null> = new BehaviorSubject<GBIFPageableResult<GbifOccurrence> | null>(null);
   public get lastSearch$() : Observable<any> {
     return this._lastSearch$;
   }
@@ -31,12 +33,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => this.emitPosition(position), (err) => { console.error(err) });
     }
 
+    // TODO on search, might be cool to scrape the web for results on most common name associated with scientific name if i cant find an official mapping 
     this._positionEmitter$.pipe(
       tap((pos) => console.log(pos)),
-      switchMap((pos: GeolocationPosition) => this._gbifService.searchNativePlants(pos.coords.latitude, pos.coords.longitude)),
+      // switchMap((pos: GeolocationPosition) => this._gbifService.searchNativePlants(pos.coords.latitude, pos.coords.longitude)), TODO undo
+      switchMap((pos: GeolocationPosition) => this._gbifService.searchNativePlants(33.853007035605174, -117.93842501103482)),
       takeUntil(this._ngDestroy$)
     ).subscribe({
-      next: (value: any) => {
+      next: (value: GBIFPageableResult<GbifOccurrence>) => {
         this._lastSearch$.next(value);
         console.log(value);
       },
