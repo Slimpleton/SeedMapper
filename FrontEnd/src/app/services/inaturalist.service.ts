172561aefeef9dbd04eb6a64a3009e5c1b8b6f1f
Slimpleton <daniel.gione@gmail.com>
2026-03-31 14:04:42 -0700

    TODO

--- FrontEnd/src/app/services/inaturalist.service.ts
+++ FrontEnd/src/app/services/inaturalist.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class InaturalistService {
+export class INaturalistService {
   // TODO make 24 hour refresh auth token for things
-    
+  // TODO pull images from the amazon bucket for freesies
+  // profit
 }
